198dae203a94cc1dd3896a7e185a9ce623c8a417
heeryoung <winterbooks@naver.com>
2022-12-07 15:15:24 +0900

    삭제 버튼 수정

--- TO-DO-LIST/src/components/Todolist.js
+++ TO-DO-LIST/src/components/Todolist.js
@@ -8,7 +8,7 @@ function Todo (props) {
           <p className="todo-content">{props?.list?.content}</p>
         </div>
         <div className="todo-button">
-          <button onClick={() => props.deleteHandler(props.list.id)} className="deleteBtn">삭제하기</button>
+          <button onClick={() => props.deleteHandler(props.list.id)} className="deleteBtn">삭제</button>
           <button onClick={() => props.doneHandler(props.list.id)} className="doneBtn">완료</button>
         </div>
       </div>
@@ -23,7 +23,7 @@ function Todo (props) {
           <p className="todo-content">{props?.list?.content}</p>
         </div>
         <div className="todo-button">
-          <button onClick={() => props.deleteHandler(props.list.id)} className="deleteBtn">삭제하기</button>
+          <button onClick={() => props.deleteHandler(props.list.id)} className="deleteBtn">삭제</button>
           <button onClick={() => props.cancelHandler(props.list.id)} className="cancelBtn">취소</button>
         </div>
       </div>
